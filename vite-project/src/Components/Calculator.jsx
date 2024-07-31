2c38e45ba88c32f33cc0965887564761d7fa73a8
DevRamona <r.ingabire@alustudent.com>
2024-07-31 11:35:02 +0200

    calculator logic

--- vite-project/src/Components/Calculator.jsx
+++ vite-project/src/Components/Calculator.jsx
@@ -1,30 +1,6 @@
 const Calculator = () => {
     return (
-        
-
-<div className="grid grid-cols-4 grid-rows-6 border w-72 text-2xl text-center">
-    <div className="p-6 border bg-slate-600 col-span-4"><button></button></div>
-    <div className="bg-gray-400 p-6 border row-start-2"> <button>AC</button></div>
-    <div className="bg-gray-400 p-6 border row-start-2"> <button>+/-</button></div>
-    <div className="bg-gray-400 p-6 border row-start-2"> <button>%</button></div>
-    <div className="bg-orange-400 p-6 border row-start-2"> <button>÷</button></div>
-    <div className="bg-orange-400 p-6 border"><button>7</button></div>
-    <div className="bg-gray-400 p-6 border row-start-3"><button>8</button></div>
-    <div className="bg-gray-400 p-6 border row-start-3"><button>9</button></div>
-    <div className="bg-gray-400 p-6 border row-start-3"><button>x</button></div>
-    <div className="bg-orange-400 p-6 border col-start-4 row-start-4"> <button>4</button></div>
-    <div className="bg-gray-400 p-6 border col-start-3 row-start-4"> <button>5</button></div>
-    <div className="bg-gray-400 p-6 border col-start-2 row-start-4"> <button>6</button></div>
-    <div className="bg-gray-400 p-6 border col-start-1 row-start-4"> <button>-</button></div>
-    <div className="bg-orange-400 p-6 border"><button>1</button></div>
-    <div className="bg-gray-400 p-6 border row-start-5"><button>2</button></div>
-    <div className="bg-gray-400 p-6 border row-start-5"><button>3</button></div>
-    <div className="bg-gray-400 p-6 border row-start-5"><button>+</button></div>
-    <div className="bg-gray-400 p-6 border col-span-2"><button>0</button></div>
-    <div className="bg-gray-400 p-6 border col-start-3 row-start-6"><button>.</button></div>
-    <div className="bg-orange-400 p-6 border col-start-4 row-start-6"><button>=</button></div>
-</div>
-    
+        <div></div>
     )
 }
 export default Calculator
